3d1db067b08397857b01f60de9fa961984f86f03
BWKenobi <boriszhuravlev@gmail.com>
2025-10-05 10:54:09 +0300

    Change deadline date

--- gavrilov/utils.py
+++ gavrilov/utils.py
@@ -6,7 +6,7 @@ def base_context(request):
 	start = False
 
 	dte = date.today()
-	dte_deadline = date(2025,10,6)
+	dte_deadline = date(2025,10,10)
 	register_block = True
 	if dte<dte_deadline:
 		register_block = False
